1e409c31fa397d5bf1160802af9794146bd9bfa4
jack <meowjack1@gmail.com>
2025-08-26 02:38:54 +0800

    add argument handler, add error typing handler, add new option for the code

--- auto_type.cpp
+++ auto_type.cpp
@@ -5,9 +5,12 @@
 #include <cpr/cpr.h>
 #include <thread>
 #include <chrono>
-
+#include <fstream>
+#include <vector>
 
 int encrypt(long long b, int e, int m);
+int argv_handle(int argc, char** argv);
+void bad_argument();
 
 const auto user_agent=cpr::Header{
     {"User-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15"}
@@ -15,10 +18,14 @@ const auto user_agent=cpr::Header{
 
 class type_handler{
     public:
-        type_handler(std::string id, std::string pw){
+        type_handler(std::string id, std::string pw, int control_code, std::string file){
+            this->control_code=control_code;
             this->id=id;
             this->pw=pw;
 
+            if(control_code==1) FileHandler(file);
+            if(control_code==2) SettingHandler();
+            
             const auto SignInPayload=cpr::Multipart{
                 {"id", id},
                 {"pw", pw}
@@ -35,7 +42,10 @@ class type_handler{
         }
         
     private:
+        int control_code;
         int w_cnt, cpm, time;
+
+        int safe_c=1, debug_c=0;
 
         std::string id, pw;
         std::string class_id, txt;
@@ -129,7 +139,7 @@ class type_handler{
                     std::cout << "[error] Invalid text: " << txt << std::endl;
                     std::exit(1);
                 }else{
-                    this->TypeHandler(0, 0);
+                    this->TypeHandler(debug_c, safe_c);
                 }
             }
             return;
@@ -155,12 +165,12 @@ class type_handler{
                 {"sub_rand", sub_rand},
                 {"type", 0},
                 {"course_id", txt},
-                {"n78bdbd373e111832", 12085},
+                {"n78bdbd373e111832", encrypt(time, 2411, 95477)},
                 {"n61e771d9dd8eab3c", encrypt(w_cnt, 2411, 95477)},
                 {"n043e1a918758a12f", 0}
             };
 
-            if(safe==1) std::this_thread::sleep_for(std::chrono::seconds(65));
+            if(safe==1) std::this_thread::sleep_for(std::chrono::seconds(time+5));
 
             cpr::Header header=cpr::Header{
                 {"Referer", "https://typing.fhjh.tp.edu.tw/student/typing.php?course_id="+txt+"&class_id="+class_id},
@@ -172,24 +182,98 @@ class type_handler{
                 payload,  
                 this->PHPSessionID
             );
-            std::cout << WritePageRes.text << std::endl;
+            if(debug==1) std::cout << WritePageRes.text << std::endl;
+            ResultHandler(WritePageRes.text);
             txt.clear();
         };
+        void FileHandler(std::string file){
+            std::ifstream reader;
+            reader.open(file);
+            if(reader.fail()){
+                std::cout << "[error] file: " << file << " not found" << std::endl;
+            }
+            std::string data[2];
+            reader >> data[0];
+            reader >> data[1];
 
+            int in0=data[0].find("debug:");
+            int in1=data[1].find("safe:");
+            if(in0>10000||in1>10000){
+                std::cout << "[error] debug file argument error" << std::endl;
+                return;
+            }
+            try{
+                debug_c=data[0][in0+6]-'0';
+                safe_c=data[1][in1+5]-'0';
+                debug_c=debug_c>1? 1:debug_c;
+                safe_c=safe_c>1? 1:safe_c;
+                debug_c=debug_c<0? 0:debug_c;
+                safe_c=safe_c<0? 0:safe_c;
+            }catch(std::exception& e){
+                std::cout << e.what() << std::endl;
+                std::cout << "[error] debug file argument error" << std::endl;
+            }
+            reader.close();
+        };
+        void ResultHandler(std::string& response){
+            int index=response.find("error=");
+            if(index==-1) std::cout << "[typing] finish typing!" << std::endl;
+            else{
+                int error_code=response[index+6]-'0';
+                switch(error_code){
+                    case 1:
+                        std::cout << "[error] too many error" << std::endl;
+                        break;
+                    case 2:
+                        std::cout << "[error] too little word" << std::endl;
+                        break;
+                    case 3:
+                        std::cout << "[error] something wrong" << std::endl;
+                        break;
+                    case 4:
+                        std::cout << "[error] wpm too low" << std::endl;
+                        break;
+                    case 5:
+                        std::cout << "[error] wpm too high" << std::endl;
+                        break;
+                    case 6:
+                        std::cout << "[error] result repeated" << std::endl;
+                        break;
+                    case 7:
+                        std::cout << "[error] time<240s" << std::endl;
+                        break;
+                    case 8:
+                        std::cout << "[error] something wrong" << std::endl;
+                        break;
+                }
+            }
+        }
+        void SettingHandler(){
+            std::string r1, r2;
+            std::cout << "[setting] do you want to see the raw responce? (y/n): ";
+            std::cin >> r1;
+            std::cout << "[setting] do you want to disable safe typing? (y/n): ";
+            std::cin >> r2;
+            if(r1=="y") debug_c=1;
+            if(r2=="y") safe_c=0;
+        }
 };
 
 
-int main(){
-    std::string str[7];
+int main(int argc, char** argv){
+    std::string file="";
+    int code=argv_handle(argc, argv);
+    if(code==1) file=argv[2];
 
-    str[1]="  _____          _     _                           _          _                    ";
-    str[2]=" |  ___|   _ ___| |__ (_)_ __   __ _    __ _ _   _| |_ ___   | |_ _   _ _ __   ___ ";
-    str[3]=" | |_ | | | / __| '_ \\| | '_ \\ / _` |  / _` | | | | __/ _ \\  | __| | | | '_ \\ / _ \\";
-    str[4]=" |  _|| |_| \\__ \\ | | | | | | | (_| | | (_| | |_| | || (_) | | |_| |_| | |_) |  __/";
-    str[5]=" |_|   \\__,_|___/_| |_|_|_| |_|\\__, |  \\__,_|\\__,_|\\__\\___/   \\__|\\__, | .__/ \\___|";
-    str[6]="                               |___/                              |___/|_|         ";
+    std::string str[6];
+    str[0]="  _____          _     _                           _          _                    ";
+    str[1]=" |  ___|   _ ___| |__ (_)_ __   __ _    __ _ _   _| |_ ___   | |_ _   _ _ __   ___ ";
+    str[2]=" | |_ | | | / __| '_ \\| | '_ \\ / _` |  / _` | | | | __/ _ \\  | __| | | | '_ \\ / _ \\";
+    str[3]=" |  _|| |_| \\__ \\ | | | | | | | (_| | | (_| | |_| | || (_) | | |_| |_| | |_) |  __/";
+    str[4]=" |_|   \\__,_|___/_| |_|_|_| |_|\\__, |  \\__,_|\\__,_|\\__\\___/   \\__|\\__, | .__/ \\___|";
+    str[5]="                               |___/                              |___/|_|         ";
 
-    for(int i=1; i<=6; i++){
+    for(int i=0; i<6; i++){
         std::cout << str[i] << std::endl;
     }
 
@@ -199,6 +283,26 @@ int main(){
     std::cout << "pw: ";
     std::cin >> pw;
 
-    auto typing=type_handler(id, pw);
+    auto typing=type_handler(id, pw, code, file);
     return 0;
+}
+
+int argv_handle(int argc, char** argv){
+    if(argc>1){
+        std::string arg1=argv[1];
+        if(argc==2) return -1;
+        else if(argc==3){
+            if(arg1=="-d") return 1;
+            else if(arg1=="-s") return 2;
+            else return -1;
+        }
+    }else{
+        return 0;
+    }
+    return 0;
+}
+
+void bad_argument(){
+    std::cout << "[error] bad argument" << std::endl;
+    std::cout << "[output] continue normal procedure" << std::endl;
 }
